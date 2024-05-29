7af0ce81744072a97abf94496d9c3288c232ca84
Dannyblazer <nnajidanny004@gmail.com>
2024-05-29 22:31:50 +0100

    User model setup

--- socialblog/user/models.py
+++ socialblog/user/models.py
@@ -1,3 +1,73 @@
+import email
+import uuid
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager as BUM
 from django.db import models
 
+from common.models import BaseModel
 # Create your models here.
+
+class BaseUserManager(BUM):
+    def create_user(self, email, is_active=True, is_admin=False, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        
+        user = self.model(
+            email=self.normalize_email(email.lower()),
+            is_active=is_active,
+            is_admin=is_admin,
+        )
+
+        if password is not None:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
+        user.full_clean()
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email=email,
+            is_active=True,
+            is_admin=True,
+            password=password,
+        )
+
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+    
+
+class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+
+    is_active=models.BooleanField(default=True)
+    is_admin=models.BooleanField(default=False)
+
+    # This should potentially be an encrypted field
+    jwt_key = models.UUIDField(default=uuid.uuid4)
+
+    objects = BaseUserManager()
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.email
+    
+    def is_staff(self):
+        return self.is_admin
+    
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(BaseUser, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
