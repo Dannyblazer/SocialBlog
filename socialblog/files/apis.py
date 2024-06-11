1a3cbce2ea1fadd36f5fff4bc8c1e52e7e18c975
Dannyblazer <nnajidanny004@gmail.com>
2024-06-11 15:41:40 +0100

    files api setup

---
+++ socialblog/files/apis.py
@@ -0,0 +1,6 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from 
