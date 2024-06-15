8582438a6594d4d942c6bde4276c1b5ecbe69c0d
Dannyblazer <nnajidanny004@gmail.com>
2024-06-15 10:20:55 +0100

    secondary root api routing

--- socialblog/socialblog/api/urls.py
+++ socialblog/socialblog/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path("auth/", include(("styleguide_example.authentication.urls", "authentication"))),
-    path("users/", include(("styleguide_example.users.urls", "users"))),
-    path("errors/", include(("styleguide_example.errors.urls", "errors"))),
-    path("files/", include(("styleguide_example.files.urls", "files"))),
+    path("auth/", include(("authentication.urls", "authentication"))),
+    path("users/", include(("user.urls", "users"))),
+    path("errors/", include(("errors.urls", "errors"))),
+    path("files/", include(("files.urls", "files"))),
     path(
-        "google-oauth2/", include(("styleguide_example.blog_examples.google_login_server_flow.urls", "google-oauth2"))
+        "google-oauth2/", include(("blog_examples.google_login_server_flow.urls", "google-oauth2"))
     ),
 ]
