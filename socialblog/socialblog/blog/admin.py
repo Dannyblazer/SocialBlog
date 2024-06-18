7f4c58d743ee8afcd8b6a07370ba8e0bdceaebab
Dannyblazer <nnajidanny004@gmail.com>
2024-06-18 23:17:12 +0100

    registered the blog admin

--- socialblog/socialblog/blog/admin.py
+++ socialblog/socialblog/blog/admin.py
@@ -1,3 +1,15 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
 
+from blog.models import Blog
 # Register your models here.
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ["id", "author", "title", "created_at", "updated_at"]
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request)
+    
