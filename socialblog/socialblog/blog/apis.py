a961b261acf3d43d3046843add144a854825424b
Dannyblazer <nnajidanny004@gmail.com>
2024-06-18 23:18:23 +0100

    implemented blog CRU operations

--- socialblog/socialblog/blog/apis.py
+++ socialblog/socialblog/blog/apis.py
@@ -1,40 +1,120 @@
+from pyexpat import model
+from tokenize import Comment
 from django.conf import settings
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers, status
 from rest_framework.response import Response
 
+from api.mixins import ApiAuthMixin
+from user.models import BaseUser
+from .selectors import blog_list, blog_get
+from api.pagination import (
+    LimitOffsetPagination,
+    get_paginated_response,
+)
 from .models import Blog
-from .services import blog_create, blog_update
+from .services import blog_create, blog_update, comment_create
 
 
 
-class BlogCreateApi(APIView):
+class BlogCreateApi(ApiAuthMixin, APIView):
     class InputSerializer(serializers.Serializer):
-        author = serializers.SlugRelatedField(
-            slug_field='email',
-            queryset=settings.AUTH_USER_MODEL.objects.all()
-        )
+        
+        title = serializers.CharField(max_length=255)
+        body = serializers.CharField()
+        
+    def post(self, request):
+        user = request.user
+        input_serializer = self.InputSerializer(data=request.data)
+        input_serializer.is_valid(raise_exception=True)
+
+        blog_create(
+                author=user,
+                title=input_serializer.validated_data['title'],
+                body=input_serializer.validated_data['body']
+                )
+
+        return Response(status=status.HTTP_201_CREATED)
+
+
+
+class BlogUpdateApi(ApiAuthMixin, APIView):
+
+    class InputSerializer(serializers.Serializer):
         title = serializers.CharField(max_length=50)
         body = serializers.CharField()
         like = serializers.IntegerField(required=False, allow_null=True)
 
-        def create(self, validated_data):
-            return Blog.objects.create(**validated_data)
-
-    class OutputSerializer(serializers.Serializer):
-        id = serializers.IntegerField()
-        author = serializers.CharField(source='author.email')
-        title = serializers.CharField()
-        body = serializers.CharField()
-        like = serializers.IntegerField(required=False, allow_null=True)
-
-    def post(self, request):
+    class OutputSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Blog
+            fields = ("id", "author", "title", "body", "like")
+    
+    def patch(self, request, blog_id):
         input_serializer = self.InputSerializer(data=request.data)
         input_serializer.is_valid(raise_exception=True)
+        blog = blog_get(blog_id)
 
-        blog = input_serializer.save()
+        if request.user == blog.author:
+            
+            new_blog, update_status = blog_update(blog=blog, data=input_serializer.validated_data)
+            output = self.OutputSerializer(new_blog)
+            # Remove the update response later
+            return Response({"update_status": update_status, "data": output.data}, status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-        output_serializer = self.OutputSerializer(blog)
-        return Response(output_serializer.data, status=status.HTTP_201_CREATED)
 
 
+class BlogListApi(ApiAuthMixin, APIView):
+    class Pagination(LimitOffsetPagination):
+        default_limit = 3
+
+    class InputSerializer(serializers.Serializer):
+        id = serializers.IntegerField(required=False)
+        author = serializers.SlugRelatedField(
+            slug_field='author',
+            queryset=BaseUser.objects.all(),
+            required=False
+        )
+        title = serializers.CharField(max_length=100, required=False)
+
+    class OutputSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Blog
+            fields = "__all__"
+
+    def get(self, request):
+        input_serializer = self.InputSerializer(data=request.query_params)
+        input_serializer.is_valid(raise_exception=True)
+
+        blogs = blog_list(filters=input_serializer.validated_data)
+
+        return get_paginated_response(
+            pagination_class=self.Pagination,
+            serializer_class=self.OutputSerializer,
+            queryset=blogs,
+            request=request,
+            view=self,
+        )
+
+
+
+class CommentCreateApi(ApiAuthMixin, APIView):
+    class InputSerializer(serializers.Serializer):
+        body = serializers.CharField()
+
+    class OutputSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Comment
+            fields = "__all__"
+
+    def post(self, request, blog_id):
+        input_serializer = self.InputSerializer(data=request.data)
+        input_serializer.is_valid(raise_exception=True)
+        
+        blog = blog_get(blog_id)
+        comment_create(owner=request.user, post=blog, body=input_serializer.validated_data['body'])
+
+        return Response(status=status.HTTP_201_CREATED)
+    
