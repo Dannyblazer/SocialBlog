0968129dea3bf143e0bee70b63dd2b9f84375dce
Dannyblazer <nnajidanny004@gmail.com>
2024-06-18 23:14:19 +0100

    added the blog app url

--- socialblog/socialblog/api/urls.py
+++ socialblog/socialblog/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("auth/", include(("authentication.urls", "authentication"))),
+    path("blogs/", include(("blog.urls", "blogs"))),
     path("users/", include(("user.urls", "users"))),
     path("errors/", include(("errors.urls", "errors"))),
     path("files/", include(("files.urls", "files"))),
