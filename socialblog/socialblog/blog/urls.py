83c00dde2c3db1e469491610b70687aa0ce71f8c
Dannyblazer <nnajidanny004@gmail.com>
2024-06-19 23:54:50 +0100

    Added comments and blog delete routes

--- socialblog/socialblog/blog/urls.py
+++ socialblog/socialblog/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .apis import BlogCreateApi, BlogUpdateApi, BlogListApi
+from .apis import BlogCreateApi, BlogUpdateApi, BlogListApi, BlogDeleteApi, CommentCreateApi, CommentListApi
 
 
 urlpatterns = [
     path("", BlogListApi.as_view(), name="create-list"),
+    path("comment/<int:blog_id>", CommentCreateApi.as_view(), name="comment"),
+    path("comment/list/<int:blog_id>", CommentListApi.as_view(), name="comment-list"),
     path("create", BlogCreateApi.as_view(), name="create-blog"),
+    path("delete/<int:blog_id>", BlogDeleteApi.as_view(), name="delete-blog"),
     path("update/<int:blog_id>", BlogUpdateApi.as_view(), name="update-blog"),
 ]
