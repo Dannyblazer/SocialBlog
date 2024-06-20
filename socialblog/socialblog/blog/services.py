7463fd652a4454b046c56d22ccafbe5365f8126f
Dannyblazer <nnajidanny004@gmail.com>
2024-06-20 23:32:57 +0100

    Blog create and like refactored and implemented

--- socialblog/socialblog/blog/services.py
+++ socialblog/socialblog/blog/services.py
@@ -1,7 +1,8 @@
 from typing import Optional
+from xmlrpc.client import Boolean
 from django.db import transaction
 
-from.models import Blog, Comment
+from.models import Blog, Comment, Like
 from common.services import model_update
 from user.models import BaseUser
 
@@ -10,14 +11,13 @@ from user.models import BaseUser
 def blog_create(
     *, author: BaseUser, title: str, body: str
 ) -> Blog:
-    blog = Blog.objects.create(author=author, title=title, body=body)
 
-    return blog
+    return Blog.objects.create(author=author, title=title, body=body)
 
 
 @transaction.atomic
 def blog_update(*, blog: Blog, data) -> Blog:
-    non_side_effect_fields = ["title", "body", "like"]
+    non_side_effect_fields = ["title", "body"]
 
     user, has_updated = model_update(instance=blog, fields=non_side_effect_fields, data=data)
 
@@ -25,13 +25,25 @@ def blog_update(*, blog: Blog, data) -> Blog:
 
     # ... some additional tasks with the user ...
 
-    return user, has_updated
+    return user
 
+
+def blog_delete(blog):
+    return blog.delete()
 
 
 def comment_create(
     *, owner: BaseUser, post: Blog, body: str
 ) -> Blog:
-    comment = Comment.objects.create(owner=owner, post=post, body=body)
 
-    return comment
+    return Comment.objects.create(owner=owner, post=post, body=body)
+
+@transaction.atomic
+def blog_like(user: BaseUser, blog: Blog) -> bool:
+    like, created = Like.objects.get_or_create(blog=blog)
+    if not like.users.filter(email=user.email).exists():
+        like.users.add(user)
+        like.full_clean()
+        like.save()
+        return True
+    return False
