8d3f075e87e51dc66367b5a37ddab019ab13a195
Dannyblazer <nnajidanny004@gmail.com>
2024-06-20 23:33:17 +0100

    Blog like route included

--- socialblog/socialblog/blog/urls.py
+++ socialblog/socialblog/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .apis import BlogCreateApi, BlogUpdateApi, BlogListApi, BlogDeleteApi, CommentCreateApi, CommentListApi
+from .apis import (
+            BlogCreateApi, BlogUpdateApi, BlogListApi,
+            BlogDeleteApi, CommentCreateApi, CommentListApi, BlogLikeApi)
 
 
 urlpatterns = [
@@ -8,5 +10,6 @@ urlpatterns = [
     path("comment/list/<int:blog_id>", CommentListApi.as_view(), name="comment-list"),
     path("create", BlogCreateApi.as_view(), name="create-blog"),
     path("delete/<int:blog_id>", BlogDeleteApi.as_view(), name="delete-blog"),
+    path("like/<int:blog_id>", BlogLikeApi.as_view(), name="like-blog"),
     path("update/<int:blog_id>", BlogUpdateApi.as_view(), name="update-blog"),
 ]
