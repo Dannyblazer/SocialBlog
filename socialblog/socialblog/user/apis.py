d3e4134f4e2b113c8bf06743ca9a57fc2519773c
Dannyblazer <nnajidanny004@gmail.com>
2024-06-21 16:10:58 +0100

    User apis

--- socialblog/socialblog/user/apis.py
+++ socialblog/socialblog/user/apis.py
@@ -1,18 +1,66 @@
-from rest_framework import serializers
+#import email
+from rest_framework import serializers, status
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from api.mixins import ApiAuthMixin
 
-from styleguide_example.api.pagination import (
+from api.pagination import (
     LimitOffsetPagination,
     get_paginated_response,
 )
 from user.models import BaseUser
 from user.selectors import user_list
+from user.services import user_create, user_update
 
 
-# TODO: When JWT is resolved, add authenticated version
-class UserListApi(APIView):
+#TODO: When JWT is resolved, add authenticated version
+
+class UserCreateApi(APIView):
+    class InputSerializer(serializers.Serializer):
+        email = serializers.EmailField()
+        password = serializers.CharField(write_only=True)
+
+    class OutputSerializer(serializers.Serializer):
+        id = serializers.IntegerField()
+        email = serializers.EmailField()
+        is_admin = serializers.BooleanField()
+
+    def post(self, request):
+        input_serializer = self.InputSerializer(data=request.data)
+        input_serializer.is_valid(raise_exception=True)
+        user = user_create(
+                email=input_serializer.validated_data['email'],
+                password=input_serializer.validated_data['password']
+            )
+        output_serializer = self.OutputSerializer(user)
+
+        return Response({"detail": output_serializer.data}, status=status.HTTP_201_CREATED)
+
+
+
+class UserUpdateApi(ApiAuthMixin, APIView):
+    class InputSerializer(serializers.Serializer):
+        email = serializers.EmailField()
+
+    class OutputSerializer(serializers.Serializer):
+        id = serializers.IntegerField()
+        email = serializers.EmailField()
+
+    def patch(self, request):
+        # Get authentication fixed
+        user = request.user
+        input_serializer = self.InputSerializer(data=request.data)
+        input_serializer.is_valid(raise_exception=True)
+
+        new_user = user_update(user=user, data=input_serializer.validated_data)
+        output_serializer = self.OutputSerializer(new_user)
+        return Response({"detail" : output_serializer.data}, status=status.HTTP_200_OK) 
+
+
+
+class UserListApi(ApiAuthMixin, APIView):
     class Pagination(LimitOffsetPagination):
-        default_limit = 1
+        default_limit = 3
 
     class FilterSerializer(serializers.Serializer):
         id = serializers.IntegerField(required=False)
