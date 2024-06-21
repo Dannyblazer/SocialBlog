fd66c2a6120efbfec43e9d7436864d135bac82c7
Dannyblazer <nnajidanny004@gmail.com>
2024-06-21 16:12:45 +0100

    user api routes

--- socialblog/socialblog/user/urls.py
+++ socialblog/socialblog/user/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
-from .apis import UserListApi
+from .apis import UserListApi, UserCreateApi, UserUpdateApi
 
-urlpatterns = [path("", UserListApi.as_view(), name="list")]
+urlpatterns = [
+        path("", UserCreateApi.as_view(), name="create"),
+        path("list", UserListApi.as_view(), name="list"),
+        path("update", UserUpdateApi.as_view(), name="update"),
+        
+            ]
