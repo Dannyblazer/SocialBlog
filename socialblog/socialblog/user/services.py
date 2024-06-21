8236b29276c45fb93ed3f85c3e34b9e75bb09862
Dannyblazer <nnajidanny004@gmail.com>
2024-06-21 16:12:27 +0100

    user app services

--- socialblog/socialblog/user/services.py
+++ socialblog/socialblog/user/services.py
@@ -1,4 +1,27 @@
-from typing import Any, Dict, List, Tuple
-from django.db import models
-from django.utils import timezone
+from typing import Optional
 
+from django.db import transaction
+
+from common.services import model_update
+from user.models import BaseUser
+
+
+def user_create(
+    *, email: str, is_active: bool = True, is_admin: bool = False, password: Optional[str] = None
+) -> BaseUser:
+    user = BaseUser.objects.create_user(email=email, is_active=is_active, is_admin=is_admin, password=password)
+
+    return user
+
+
+@transaction.atomic
+def user_update(*, user: BaseUser, data) -> BaseUser:
+    non_side_effect_fields = ["email", "last_name"]
+
+    user, has_updated = model_update(instance=user, fields=non_side_effect_fields, data=data)
+
+    # Side-effect fields update here (e.g. username is generated based on first & last name)
+
+    # ... some additional tasks with the user ...
+
+    return user
