5ca5ffb4c4f58d939677c3765e66b24468243bf4
Dannyblazer <nnajidanny004@gmail.com>
2024-06-22 21:52:52 +0100

    rearranged routes

--- socialblog/socialblog/blog/urls.py
+++ socialblog/socialblog/blog/urls.py
@@ -6,11 +6,11 @@ from .apis import (
 
 urlpatterns = [
     path("", BlogListApi.as_view(), name="create-list"),
-    path("comment/<int:blog_id>", CommentCreateApi.as_view(), name="comment"),
+    path("comment/<int:blog_id>", CommentCreateApi.as_view(), name="comment-create"),
     path("comment/list/<int:blog_id>", CommentListApi.as_view(), name="comment-list"),
-    path("comment/delete/<int:blog_id>", CommentDeleteApi.as_view(), name="delete-comment"),
+    path("comment/delete/<int:comment_id>", CommentDeleteApi.as_view(), name="delete-comment"),
     path("create", BlogCreateApi.as_view(), name="create-blog"),
     path("delete/<int:blog_id>", BlogDeleteApi.as_view(), name="delete-blog"),
     path("like/<int:blog_id>", BlogLikeApi.as_view(), name="like-blog"),
-    path("update/<int:blog_id>", BlogUpdateApi.as_view(), name="update-blog"),
+    path("<int:blog_id>", BlogUpdateApi.as_view(), name="update-blog"),
 ]
