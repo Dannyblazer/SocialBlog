0b1a9a2a33c4a9f858fc93ee9da662c42bd7982a
Dannyblazer <nnajidanny004@gmail.com>
2024-06-30 23:44:03 +0100

    implemented Follow sysrem selectors

--- socialblog/socialblog/user/selectors.py
+++ socialblog/socialblog/user/selectors.py
@@ -1,7 +1,11 @@
+from django.apps import apps
 from django.db.models.query import QuerySet
+from django.db.models import Q
+from .models import BaseUser, Follow
 
 from user.filters import BaseUserFilter
-from user.models import BaseUser
+
+
 
 
 def user_get_login_data(*, user: BaseUser):
@@ -20,3 +24,34 @@ def user_list(*, filters=None) -> QuerySet[BaseUser]:
     qs = BaseUser.objects.all()
 
     return BaseUserFilter(filters, qs).qs
+
+
+
+def get_following(user):
+    return Follow.objects.filter(follower=user, status=Follow.STATUS.ACCEPTED).select_related('followed')
+
+def get_followers(user):
+    return Follow.objects.filter(followed=user, status=Follow.STATUS.ACCEPTED).select_related('follower')
+
+def get_pending_follow_requests(user):
+    return Follow.objects.filter(followed=user, status=Follow.STATUS.PENDING).select_related('follower')
+
+def is_following(user, other_user):
+    return Follow.objects.filter(follower=user, followed=other_user, status=Follow.STATUS.ACCEPTED).exists()
+
+def follow_user(follower, followed):
+    if not is_following(follower, followed):
+        Follow.objects.create(follower=follower, followed=followed, status=Follow.STATUS.PENDING)
+
+def unfollow_user(follower, followed):
+    Follow.objects.filter(follower=follower, followed=followed).delete()
+
+def accept_follow_request(followed, follower):
+    follow_request = Follow.objects.filter(follower=follower, followed=followed, status=Follow.STATUS.PENDING).first()
+    if follow_request:
+        follow_request.status = Follow.STATUS.ACCEPTED
+        follow_request.save()
+
+def decline_follow_request(followed, follower):
+    Follow.objects.filter(follower=follower, followed=followed, status=Follow.STATUS.PENDING).delete()
+
