8b7ecb7320f7078ba8d620916b5aa4146393be14
Dannyblazer <nnajidanny004@gmail.com>
2024-06-30 23:44:33 +0100

    removed default fields

--- socialblog/socialblog/user/services.py
+++ socialblog/socialblog/user/services.py
@@ -7,9 +7,9 @@ from user.models import BaseUser
 
 
 def user_create(
-    *, email: str, username: str, is_active: bool = True, is_admin: bool = False, password: Optional[str] = None
+    *, email: str, username: str, password: Optional[str] = None
 ) -> BaseUser:
-    user = BaseUser.objects.create_user(email=email, username=username, is_active=is_active, is_admin=is_admin, password=password)
+    user = BaseUser.objects.create_user(email=email, username=username, password=password)
 
     return user
 
