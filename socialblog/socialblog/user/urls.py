ff3101cb4d311d41ed2a1e216abdcb1cd445a027
Dannyblazer <nnajidanny004@gmail.com>
2024-07-04 00:01:38 +0100

    Added follow feature routes

--- socialblog/socialblog/user/urls.py
+++ socialblog/socialblog/user/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from .apis import UserListApi, UserCreateApi, UserUpdateApi
+from .apis import (UserListApi, UserCreateApi, UserUpdateApi, UserFollowApi,
+                   UserUnFollowApi, UserFollowerListApi, UserFollowingListApi)
 
 urlpatterns = [
         path("", UserCreateApi.as_view(), name="create"),
+        path("follow/<int:user_id>", UserFollowApi.as_view(), name="follow"),
+        path("followers", UserFollowerListApi.as_view(), name="followers"),
+        path("following/<int:user_id>", UserFollowingListApi.as_view(), name="following"),
         path("list", UserListApi.as_view(), name="list"),
+        path("unfollow/<int:user_id>", UserUnFollowApi.as_view(), name="unfollow"),
         path("update", UserUpdateApi.as_view(), name="update"),
         
             ]
