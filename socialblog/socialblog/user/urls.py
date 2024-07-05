cc85ed1d22dd8111ae2b98b8c478d007887d49b3
Dannyblazer <nnajidanny004@gmail.com>
2024-07-05 01:04:19 +0100

    Add the request list route

--- socialblog/socialblog/user/urls.py
+++ socialblog/socialblog/user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .apis import (UserListApi, UserCreateApi, UserUpdateApi, UserFollowApi,
-                   UserUnFollowApi, UserFollowerListApi, UserFollowingListApi)
+                   UserUnFollowApi, UserFollowerListApi, UserFollowingListApi, UserFollowRequestListApi)
 
 urlpatterns = [
         path("", UserCreateApi.as_view(), name="create"),
@@ -9,6 +9,7 @@ urlpatterns = [
         path("followers", UserFollowerListApi.as_view(), name="followers"),
         path("following/<int:user_id>", UserFollowingListApi.as_view(), name="following"),
         path("list", UserListApi.as_view(), name="list"),
+        path("requests", UserFollowRequestListApi.as_view(), name="requests"),
         path("unfollow/<int:user_id>", UserUnFollowApi.as_view(), name="unfollow"),
         path("update", UserUpdateApi.as_view(), name="update"),
         
