1431be7c29a033b8438b87bd0ac7892de3a36089
Dannyblazer <nnajidanny004@gmail.com>
2024-07-06 23:20:45 +0100

    point to the accept and decline api views

--- socialblog/socialblog/user/urls.py
+++ socialblog/socialblog/user/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from .apis import (UserListApi, UserCreateApi, UserUpdateApi, UserFollowApi,
-                   UserUnFollowApi, UserFollowerListApi, UserFollowingListApi, UserFollowRequestListApi)
+                   UserUnFollowApi, UserFollowerListApi, UserFollowingListApi,
+                   UserFollowRequestListApi, UserRequestAcceptApi, UserRequestDeclineApi)
 
 urlpatterns = [
         path("", UserCreateApi.as_view(), name="create"),
+        path("accept/<int:request_id>", UserRequestAcceptApi.as_view(), name="accept-request"),
+        path("decline/<int:request_id>", UserRequestDeclineApi.as_view(), name="decline-request"),
         path("follow/<int:user_id>", UserFollowApi.as_view(), name="follow"),
         path("followers", UserFollowerListApi.as_view(), name="followers"),
         path("following/<int:user_id>", UserFollowingListApi.as_view(), name="following"),
