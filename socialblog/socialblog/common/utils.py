429939ebb8ca7bac945b2b713ba082edfe1c3043
Dannyblazer <nnajidanny004@gmail.com>
2024-07-07 23:03:38 +0100

    refactored and added the necessary paramter to the upload_location funtion

--- socialblog/socialblog/common/utils.py
+++ socialblog/socialblog/common/utils.py
@@ -1,8 +1,9 @@
 
-def upload_location(self):
+def upload_location(self, filename):
     """Generator function for image upload path"""
-    return f"profile_images/{self.pk}"
+    return 'profile_images/' + str(self.pk) + '/profile_image.png'
 
 # Return a default image path incase no image was uploaded
 def default_image():
-    return "default image path"
+    return 'account/nnajidanny004@gmail.com/20221119_205632.jpg'
+
