50882567baae17cd3d1277b95c2b2acbb7890ffb
Dannyblazer <nnajidanny004@gmail.com>
2024-07-07 23:06:33 +0100

    added the profile views to the urls.

--- socialblog/socialblog/user/urls.py
+++ socialblog/socialblog/user/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from .apis import (UserListApi, UserCreateApi, UserUpdateApi, UserFollowApi,
                    UserUnFollowApi, UserFollowerListApi, UserFollowingListApi,
-                   UserFollowRequestListApi, UserRequestAcceptApi, UserRequestDeclineApi)
+                   UserFollowRequestListApi, UserRequestAcceptApi, UserRequestDeclineApi,
+                   ProfileViewApi, ProfileUpdateApi)
 
 urlpatterns = [
         path("", UserCreateApi.as_view(), name="create"),
@@ -12,6 +13,8 @@ urlpatterns = [
         path("followers", UserFollowerListApi.as_view(), name="followers"),
         path("following/<int:user_id>", UserFollowingListApi.as_view(), name="following"),
         path("list", UserListApi.as_view(), name="list"),
+        path("profile/<int:user_id>", ProfileViewApi.as_view(), name="profile"),
+        path("profile_update/<int:user_id>", ProfileUpdateApi.as_view(), name="profile-update"),
         path("requests", UserFollowRequestListApi.as_view(), name="requests"),
         path("unfollow/<int:user_id>", UserUnFollowApi.as_view(), name="unfollow"),
         path("update", UserUpdateApi.as_view(), name="update"),
