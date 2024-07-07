69b0749871e795b7c38ffc7cb4dd0bb54c48f555
Dannyblazer <nnajidanny004@gmail.com>
2024-07-07 23:05:51 +0100

    implemented a profile_update service.

--- socialblog/socialblog/user/services.py
+++ socialblog/socialblog/user/services.py
@@ -3,7 +3,7 @@ from typing import Optional
 from django.db import transaction
 
 from common.services import model_update
-from user.models import BaseUser
+from user.models import BaseUser, Profile
 
 
 def user_create(
@@ -19,6 +19,18 @@ def user_update(*, user: BaseUser, data) -> BaseUser:
     non_side_effect_fields = ["email", "username"]
 
     user, has_updated = model_update(instance=user, fields=non_side_effect_fields, data=data)
+
+    # Side-effect fields update here (e.g. username is generated based on first & last name)
+
+    # ... some additional tasks with the user ...
+
+    return user
+
+@transaction.atomic
+def profile_update(*, profile: Profile, data) -> Profile:
+    non_side_effect_fields = ["image", "bio", "location", "birth_date"]
+
+    user, has_updated = model_update(instance=profile, fields=non_side_effect_fields, data=data)
 
     # Side-effect fields update here (e.g. username is generated based on first & last name)
 
