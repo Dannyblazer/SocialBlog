c21580f26b18fe64a59b4418c70acd926b5bcc97
Dannyblazer <nnajidanny004@gmail.com>
2024-08-25 00:54:36 +0100

    create or return room api view

--- socialblog/socialblog/chat/apis.py
+++ socialblog/socialblog/chat/apis.py
@@ -9,4 +9,23 @@ from api.pagination import (
     get_paginated_response,
 )
 from .models import ChatRoom, ChatMessage, UnreadChatRoomMessages
+from user.selectors import user_get
+from .selectors import get_or_return_room
+from .services import *
 
+
+class CreateOrReturnRoom(ApiAuthMixin, APIView):
+    class OutputSerializer(serializers.Serializer):
+        room_id = serializers.IntegerField()
+
+    def post(self, request, user2_id):
+        user1 = request.user
+        user2 = user_get(user2_id)
+        room = get_or_return_room(user1, user2)
+
+        output = self.OutputSerializer(room)
+
+        return Response({"data": output.data}, status=status.HTTP_202_ACCEPTED)
+
+
+# Create the Chat Room List & Decide wether to use sockets or normal api to query chat messages
