db70a4e07a942d251ca1e9961b94faab3f3f79b0
Dannyblazer <nnajidanny004@gmail.com>
2024-08-24 18:45:22 +0100

    added a validator

--- socialblog/socialblog/chat/models.py
+++ socialblog/socialblog/chat/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
@@ -14,6 +15,10 @@ class ChatRoom(BaseModel):
     user2       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user2')
     is_active   		= models.BooleanField(default=True)
     connected_users     = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name="connected_users")
+    
+    def clean(self):
+        if self.user1 == self.user2:
+            raise ValidationError("Can't create room with yourself")        
 
     def __str__(self):
         return f"A chat between {self.user1.username} and {self.user2.username}"
@@ -62,24 +67,17 @@ class ChatMessage(BaseModel):
         return self.content
 
 
-class UnreadChatRoomMessages(models.Model):
-	"""
-	Keep track of the number of unread messages by a specific user in a specific private chat.
-	When the user connects the chat room, the messages will be considered "read" and 'count' will be set to 0.
-	"""
+class UnreadChatRoomMessages(BaseModel):
+
 	room                = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name="room")
-
 	user                = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
 	count               = models.IntegerField(default=0)
 
 	most_recent_message = models.CharField(max_length=500, blank=True, null=True)
 
-	# last time msgs were read by the user
 	reset_timestamp     = models.DateTimeField()
 
 	#notifications       = GenericRelation(Notification)
-
 
 	def __str__(self):
 		return f"Messages that {str(self.user.username)} has not read yet."
@@ -103,5 +101,64 @@ class UnreadChatRoomMessages(models.Model):
 		"""
 		if self.user == self.room.user1:
 			return self.room.user2
-		else:
-			return self.room.user1
+		return self.room.user1
+
+"""
+@receiver(post_save, sender=PrivateChatRoom)
+def create_unread_chatroom_messages_obj(sender, instance, created, **kwargs):
+	if created:
+		unread_msgs1 = UnreadChatRoomMessages(room=instance, user=instance.user1)
+		unread_msgs1.save()
+
+		unread_msgs2 = UnreadChatRoomMessages(room=instance, user=instance.user2)
+		unread_msgs2.save()
+
+
+
+@receiver(pre_save, sender=UnreadChatRoomMessages)
+def increment_unread_msg_count(sender, instance, **kwargs):
+	
+	#When the unread message count increases, update the notification. 
+	#If one does not exist, create one. (This should never happen)
+	
+	if instance.id is None: # new object will be created
+		pass # create_unread_chatroom_messages_obj will handle this scenario
+	else:
+		previous = UnreadChatRoomMessages.objects.get(id=instance.id)
+		if previous.count < instance.count: # if count is incremented
+			content_type = ContentType.objects.get_for_model(instance)
+			if instance.user == instance.room.user1:
+				other_user = instance.room.user2
+			else:
+				other_user = instance.room.user1
+			try:
+				notification = Notification.objects.get(target=instance.user, content_type=content_type, object_id=instance.id)
+				notification.verb = instance.most_recent_message
+				notification.timestamp = timezone.now()
+				notification.save()
+			except Notification.DoesNotExist:
+				instance.notifications.create(
+					target=instance.user,
+					from_user=other_user,
+					redirect_url=f"Room_id={instance.room.id}", # we want to go to the chatroom
+					verb=instance.most_recent_message,
+					content_type=content_type,
+				)
+
+
+@receiver(pre_save, sender=UnreadChatRoomMessages)
+def remove_unread_msg_count_notification(sender, instance, **kwargs):
+	
+	#If the unread messge count decreases, it means the user joined the chat. So delete the notification.
+	if instance.id is None: # new object will be created
+		pass # create_unread_chatroom_messages_obj will handle this scenario
+	else:
+		previous = UnreadChatRoomMessages.objects.get(id=instance.id)
+		if previous.count > instance.count: # if count is decremented
+			content_type = ContentType.objects.get_for_model(instance)
+			try:
+				notification = Notification.objects.get(target=instance.user, content_type=content_type, object_id=instance.id)
+				notification.delete()
+			except Notification.DoesNotExist:
+				pass
+			"""
