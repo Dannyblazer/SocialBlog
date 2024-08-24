17f24e04bb082de0f97c912cf17123bab8f4a362
Dannyblazer <nnajidanny004@gmail.com>
2024-08-25 00:53:45 +0100

    room_list and get_room selectors

--- socialblog/socialblog/chat/selectors.py
+++ socialblog/socialblog/chat/selectors.py
@@ -1,6 +1,8 @@
+from ast import Dict
+from django.db.models import Q
 from django.db.models.query import QuerySet
-from .models import ChatRoom, ChatMessage, UnreadChatRoomMessages
-from .filters import ChatFilter
+from .models import ChatMessage, ChatRoom
+from .filters import ChatFilter, RoomEncoder
 
 
 
@@ -8,4 +10,19 @@ def chat_message_list(*, filters=None) -> QuerySet[ChatMessage]:
     filters = filters or {}
     qs = ChatMessage.objects.select_related('author')
     return ChatFilter(filters, qs).qs
+
+
+def chat_room_list(request) -> Dict:
+    user = request.user
+    encoder = RoomEncoder(context={'user': user})
+    serialized_data = encoder.to_representation()
+    return serialized_data
+
+
+def get_or_return_room(user1, user2) -> ChatRoom:
+    chat, created = ChatRoom.objects.get_or_create(
+        Q(user1=user1, user2=user2) | Q(user1=user2, user2=user1),
+        defaults={'user1': user1, 'user2': user2}
+    )
+    return chat
 
