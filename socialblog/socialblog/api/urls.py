9bef050d911c98ec1e8db8d8e02f415860b2a3b4
Dannyblazer <nnajidanny004@gmail.com>
2024-08-25 22:22:07 +0100

    added chat app routes

--- socialblog/socialblog/api/urls.py
+++ socialblog/socialblog/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 urlpatterns = [
     path("auth/", include(("authentication.urls", "authentication"))),
     path("blogs/", include(("blog.urls", "blogs"))),
+    path("chat/", include(("chat.urls", "chat"))),
     path("users/", include(("user.urls", "users"))),
     path("errors/", include(("errors.urls", "errors"))),
     path("files/", include(("files.urls", "files"))),
