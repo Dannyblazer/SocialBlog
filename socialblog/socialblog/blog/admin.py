6fce774acb5c4a3b1e6e59652e6b76c8894f4598
Dannyblazer <nnajidanny004@gmail.com>
2024-08-26 22:31:26 +0100

    registered like model to admin

--- socialblog/socialblog/blog/admin.py
+++ socialblog/socialblog/blog/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.db.models.query import QuerySet
 from django.http import HttpRequest
 
-from blog.models import Blog
+from blog.models import Blog, Like
 # Register your models here.
 
 @admin.register(Blog)
@@ -13,3 +13,10 @@ class BlogAdmin(admin.ModelAdmin):
     def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
         return super().get_queryset(request)
     
+
+@admin.register(Like)
+class BlogLikeAdmin(admin.ModelAdmin):
+    list_display = ["id", "blog"]
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request)
