c22042e4bd98efec5bf375bfa4399f171abeb4e0
Dannyblazer <nnajidanny004@gmail.com>
2024-09-07 10:50:49 +0100

    added jwt to auth classes

--- socialblog/config/settings/base.py
+++ socialblog/config/settings/base.py
@@ -351,7 +351,7 @@ REST_FRAMEWORK = {
     "EXCEPTION_HANDLER": "api.exception_handlers.drf_default_with_modifications_exception_handler",
     # 'EXCEPTION_HANDLER': 'styleguide_example.api.exception_handlers.hacksoft_proposed_exception_handler',
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
-    "DEFAULT_AUTHENTICATION_CLASSES": [],
+    "DEFAULT_AUTHENTICATION_CLASSES": ['rest_framework_simplejwt.authentication.JWTAuthentication',],
 }
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
