e831c58895326884ee9db81d38161a93aa2db61c
Dannyblazer <nnajidanny004@gmail.com>
2024-09-16 09:43:50 +0100

    debug socket 403 status code cause

--- socialblog/config/asgi.py
+++ socialblog/config/asgi.py
@@ -14,6 +14,14 @@ import sys
 from pathlib import Path
 
 from django.core.asgi import get_asgi_application
+from channels.routing import URLRouter, ProtocolTypeRouter
+from channels.security.websocket import AllowedHostsOriginValidator  # new
+from django.core.asgi import get_asgi_application
+
+from django.urls import re_path
+from chat.consumers import ChatConsumer
+#from notification.consumers import NotificationConsumer
+from .tokenauth_middleware import JWTAuthMiddleware  # new
 
 # This allows easy placement of apps within the interior
 # socialblog directory.
@@ -23,21 +31,17 @@ sys.path.append(str(BASE_DIR / "socialblog"))
 # If DJANGO_SETTINGS_MODULE is unset, default to the local settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
-# This application object is used by any ASGI server configured to use this file.
-django_application = get_asgi_application()
-# Apply ASGI middleware here.
-# from helloworld.asgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+django_asgi_app = get_asgi_application()
 
-# Import websocket application here, so apps from django_application are loaded first
-from config.websocket import websocket_application
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(  
+        JWTAuthMiddleware(
+            URLRouter([
+                    re_path(r"ws/chat/$", ChatConsumer.as_asgi()),
+            ])
+        )
+    )
+})
 
-
-async def application(scope, receive, send):
-    if scope["type"] == "http":
-        await django_application(scope, receive, send)
-    elif scope["type"] == "websocket":
-        await websocket_application(scope, receive, send)
-    else:
-        msg = f"Unknown scope type {scope['type']}"
-        raise NotImplementedError(msg)
+                    #re_path(r"ws/notification/$", NotificationConsumer.as_asgi()),
