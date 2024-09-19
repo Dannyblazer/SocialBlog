85b03f3ab69be4f61ac6f46167ea5ae64945d5d1
Dannyblazer <nnajidanny004@gmail.com>
2024-09-19 09:51:02 +0100

    comsumer selectors

--- socialblog/socialblog/chat/selectors.py
+++ socialblog/socialblog/chat/selectors.py
@@ -1,7 +1,9 @@
 from ast import Dict
-from django.db.models import Q
+from django.db.models import Q, Prefetch
 from django.db.models.query import QuerySet
-from .models import ChatMessage, ChatRoom
+from channels.db import database_sync_to_async
+#from user.models import BaseUser
+from .models import ChatMessage, ChatRoom, UnreadChatRoomMessages
 from .filters import ChatFilter, RoomEncoder
 
 
@@ -26,4 +28,33 @@ def get_or_return_room(user1, user2) -> ChatRoom:
         # If the chat room doesn't exist, create it
         chat = ChatRoom.objects.create(user1=user1, user2=user2)
     return chat
+
+
+
+# Consumers Selectors
+
+@database_sync_to_async
+def get_room_or_error(room_id, user):
+    """
+    Tries to fetch a room for the user, checking permissions along the way
+    """
+    try:
+        room = ChatRoom.objects.select_related('user1', 'user2').prefetch_related(
+            Prefetch('room', queryset=UnreadChatRoomMessages.objects.filter(user=user))
+        ).get(pk=room_id)
+    except ChatRoom.DoesNotExist:
+        print("Get room or error exception!")
+        raise Exception("Invalid Room.")
+    
+    # Is this user allowed into this room?
+    if user != room.user1 and user != room.user2:
+        print("Not your chat bro!")
+        raise Exception("You do not have permission to join this room.")
+    
+    return room
+
+
+@database_sync_to_async
+def connected_users(room):
+    return room.connected_users.all()
 
