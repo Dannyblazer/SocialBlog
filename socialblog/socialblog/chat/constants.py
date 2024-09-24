dd4db450da2eb9bdfda395c8fc23d90cde63ed6f
Dannyblazer <nnajidanny004@gmail.com>
2024-09-24 22:46:26 +0100

    chat comsumer constants

---
+++ socialblog/socialblog/chat/constants.py
@@ -0,0 +1,5 @@
+DEFAULT_ROOM_CHAT_MESSAGE_PAGE_SIZE = 20
+BASE_URL = "http://127.0.0.1:8000/media"
+SG_TYPE_MESSAGE = 0 # For standard messages
+MSG_TYPE_ENTER2 = 1 # Users enters the room
+MSG_TYPE_LEAVE2 = 2 # users leaves the room
